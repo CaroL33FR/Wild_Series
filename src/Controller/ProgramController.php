7a0b52aa99ed5ca208d6e30f161ea4b830008a81
CAROLINE LEBON <lebon.caroline.lc@gmail.com>
2023-05-28 13:29:31 +0200

    added findAll and findOneBy to methods index and show respectively but  error view still to be handled

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,24 +6,34 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
-
+use App\Repository\ProgramRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-        ]);
+            'programs' => $programs,
+            ]
+        );
     }
     #[Route('/{id}', name: 'id', requirements: ['id'=>'\d+'], methods: ['GET'])]
-    public function show($id): Response
+    public function show($id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -23,6 +23,10 @@ class Program
     #[ORM\Column(type: Types::TEXT)]
     private ?string $synopsis = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +64,18 @@ class Program
     public function setSynopsis(string $synopsis): self
     {
         $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
