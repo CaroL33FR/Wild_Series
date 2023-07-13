cfcc83c77212df71b81a06149212aeddd04af1e9
CAROLINE LEBON <lebon.caroline.lc@gmail.com>
2023-07-13 17:10:54 +0200

    added actors selection to new program form

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
@@ -24,8 +26,14 @@ class ProgramType extends AbstractType
             //Quest 14: asked to write the below sentence instead of the above one
             //Create error: to be fixed later
             ->add('category', null, ['choice_label' => 'name'])
-
-        ;
+            //Quest 19: EntityType
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
